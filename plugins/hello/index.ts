85b84dbe8c1bce1774c20b977f6727331be9dc99
SelfhostedPro <info@selfhosted.pro>
2024-10-14 11:24:48 -0700

    reworking plugins

--- plugins/hello/index.ts
+++ plugins/hello/index.ts
@@ -1,7 +1,1 @@
-import { tryUseNuxt, useNuxt } from "@nuxt/kit";
-
-export const defineHostedPlugin = () => {
-    const nuxt = tryUseNuxt()
-    if (!nuxt) throw Error(`Unable to get nuxt content. This is meant to be run from another nuxt project`)
-    console.log(nuxt)
-}
+console.log("Hello via Bun!");
