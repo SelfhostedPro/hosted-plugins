c65b1067d59cb89fe3dcf9e68af23954dba3f13c
SelfhostedPro <info@selfhosted.pro>
2024-10-18 14:25:05 -0700

    fixing build

--- plugins/hello/build.config.ts
+++ plugins/hello/build.config.ts
@@ -1,10 +1,29 @@
 import { defineBuildConfig } from "unbuild";
+import { vueLoader } from "@teages/mkdist-vue-loader"
 export default defineBuildConfig({
     entries: [
         "./src/index.ts",
         {
             builder: "mkdist",
             input: "./src/app/",
+            pattern: "**/*.vue",
+            loaders: [vueLoader],
+            outDir: "./dist/app",
+        },
+        {
+            builder: "mkdist",
+            input: "./src/app/",
+            pattern: "**/*.ts",
+            format: 'cjs',
+            loaders: ['js'],
+            outDir: "./dist/app",
+        },
+        {
+            builder: "mkdist",
+            input: "./src/app/",
+            pattern: "**/*.ts",
+            format: 'esm',
+            loaders: ['js'],
             outDir: "./dist/app",
         },
         {
